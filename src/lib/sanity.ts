740f3e78622e084b1f9cb81fa9f40e8173c26e0f
lukecartledge <luke@lukecartledge.com>
2026-04-04 11:29:03 +0200

    style: format codebase with Prettier

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,8 +1,8 @@
-import { createClient } from '@sanity/client';
+import { createClient } from '@sanity/client'
 
 export const client = createClient({
   projectId: 'c42ibxe8',
   dataset: 'production',
   apiVersion: '2024-01-01',
   useCdn: false,
-});
+})
