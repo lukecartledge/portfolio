d1f990199a5afd7d480049c39fd77cbd7d86e24a
lukecartledge <luke@lukecartledge.com>
2026-04-04 18:23:38 +0200

    Remove Sanity Studio directory and client library

--- sanity/schemaTypes/index.ts
+++
@@ -1,6 +0,0 @@
-import siteConfig from './siteConfig'
-import photo from './photo'
-import collection from './collection'
-import page from './page'
-
-export const schemaTypes = [siteConfig, photo, collection, page]
